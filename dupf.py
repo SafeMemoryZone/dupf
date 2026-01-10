5b01131d10e6a92c00fbc316e30f49d5f669e8ee
Viliam Holly <viliamteoholly@gmail.com>
2026-01-10 11:40:31 +0100

    Fix hardlink and symlink bugs

--- dupf.py
+++ dupf.py
@@ -1,27 +1,49 @@
 import os
 import hashlib
+import argparse
+
 
 def sha256(filepath):
-    with open(filepath, 'rb') as f:
-        return int(hashlib.file_digest(f, 'sha256').hexdigest(), 16)
+    with open(filepath, "rb") as f:
+        return int(hashlib.file_digest(f, "sha256").hexdigest(), 16)
 
-def walk_error(error):
-    print(f'Error: {error.strerror}')
 
 def main():
+    parser = argparse.ArgumentParser(
+        prog="dupf",
+        description="Simple script that finds duplicate files.",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--root",
+        type=str,
+        help="path to the root directory (default is CWD)",
+        default=os.getcwd(),
+    )
+    args = parser.parse_args()
+
     encountered_hashes = dict()
     duplicates_size = 0
 
-    start_path = input("Start path > ");
-    for root, _, files in os.walk(start_path):
+    for root, _, files in os.walk(args.root):
         for f in files:
             path = os.path.join(root, f)
-            digest = sha256(path)
+
+            if os.path.islink(path) or os.stat(path).st_nlink > 1:
+                continue
+
+            try:
+                digest = sha256(path)
+            except PermissionError:
+                print(f"Warning: Unable to open {path} (permission error)")
+                continue
+
             if digest in encountered_hashes:
                 print(encountered_hashes[digest] + " and " + path)
                 duplicates_size += os.path.getsize(path)
             else:
-                encountered_hashes[digest] = path 
+                encountered_hashes[digest] = path
 
     units = ["b", "kb", "mb", "gb"]
     convs = [1, 1000, 1000_000, 1000_000_000]
@@ -34,9 +56,11 @@ def main():
         best = len(str(round(duplicates_size / convs[best_idx], 1)))
         if curr < best:
             best_idx = i
-    
-    print(f'\nFound ~{round(duplicates_size / convs[best_idx], 1)}{units[best_idx]} worth of duplicate files.')
+
+    print(
+        f"\nFound ~{round(duplicates_size / convs[best_idx], 1)}{units[best_idx]} worth of duplicate files."
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
